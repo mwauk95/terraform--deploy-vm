e072fff71b1a64ed0e9e67a9e502015a21cf8cca
mwauk95 <kilonzo.mwau95@gmail.com>
2025-10-15 23:37:48 +0300

    feat:Create Terraform configuration for an EC2 instance

---
+++ Terraform-iac/main.tf
@@ -0,0 +1,84 @@
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras install -y nginx1
+              sudo systemctl start nginx
+              echo "<h1>Hello World</h1>" > /usr/share/nginx/html/index.html
+              EOF
+
+  tags = {
+    Name = "WebServer"
+  }
+}
